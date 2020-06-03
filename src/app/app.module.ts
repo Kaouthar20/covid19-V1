d125a669e196b0dd05cf3692ef190ac626fdcaa4
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-03 22:21:16 +0000

    la supression des componnets indesirable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,7 @@ import { LoginComponent } from './components/contact/login/login.component';
 import { SymptomComponent } from './components/symptoms/symptom/symptom.component';
 
 
-import { HighFeverComponent } from './components/symptoms/high-fever/high-fever.component';
-import { PneumoniaComponent } from './components/symptoms/pneumonia/pneumonia.component';
-import { ShortnessOfBreathComponent } from './components/symptoms/shortness-of-breath/shortness-of-breath.component';
-import { VomitingComponent } from './components/symptoms/vomiting/vomiting.component';
-import { DiarrheaComponent } from './components/symptoms/diarrhea/diarrhea.component';
-import { CoughingsComponent } from './components/symptoms/coughings/coughings.component';
+
 import { PreventionsComponent } from './components/preventions/preventions.component';
 import { PreventionComponent } from './components/preventions/prevention/prevention.component';
 
