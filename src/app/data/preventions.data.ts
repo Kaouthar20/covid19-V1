c8533d010fada15217d9efa3bb42f89f9a053415
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-03 20:39:18 +0000

    modification de component prevention

--- src/app/data/preventions.data.ts
+++ src/app/data/preventions.data.ts
@@ -1,10 +1,10 @@
 import { Prevention } from '../objects/prevention';
 
 export const preventions =  [
-    new Prevention('Avoid Close Contact','Coughing.jpg', 'The most common symptoms of COVID-19 are fever, tiredness, and dry cough.'),
-    new Prevention('Clean Your Hands Often','Coughing.jpg', 'The most common symptoms of COVID-19 are fever, tiredness, and dry cough.'),
-    new Prevention('Stay at Home','Coughing.jpg', 'The most common symptoms of COVID-19 are fever, tiredness, and dry cough.'),
-    new Prevention('Cover Coughs and Sneezes','Coughing.jpg', 'The most common symptoms of COVID-19 are fever, tiredness, and dry cough.'),
-    new Prevention('Wear a Facemask For Safety','Coughing.jpg', 'The most common symptoms of COVID-19 are fever, tiredness, and dry cough.'),
-    new Prevention('Clean and Disinfect','Coughing.jpg', 'The most common symptoms of COVID-19 are fever, tiredness, and dry cough.')
+    new Prevention('Avoid Close Contact','Coughing.jpg', 'The virus is thought to spread mainly from person-to-person.Between people who are in close contact with one another (within about 6 feet). Droplets produced when an infected person coughs, sneezes or talks.'),
+    new Prevention('Clean Your Hands Often','Coughing.jpg', 'Wash your hands often with soap and water for at least 20 seconds especially after you have been in a public place, or after blowing your nose, coughing, or sneezing.'),
+    new Prevention('Stay at Home','Coughing.jpg', 'Avoid close contact with people who are sick Stay home as much as possible. Put distance between yourself and other people. some people without symptoms may be able to spread virus.'),
+    new Prevention('Cover Coughs and Sneezes','Coughing.jpg', 'If you are in a private setting and do not have on your cloth face covering, remember to always cover your mouth and nose with a tissue when you cough or sneeze or use the inside of your elbow.'),
+    new Prevention('Wear a Facemask For Safety','Coughing.jpg', 'You could spread COVID-19 to others even if you do not feel sick. Everyone should wear a cloth face cover when they have to go out in public, for example to the grocery store or to pick up other necessities.'),
+    new Prevention('Clean and Disinfect','Coughing.jpg', 'Clean AND disinfect frequently touched surfaces daily. This includes tables, doorknobs, light switches, countertops, handles, desks, phones, keyboards, toilets, faucets, and sinks.')
 ];
--- src/app/objects/prevention.ts
+++ src/app/objects/prevention.ts
@@ -2,7 +2,7 @@
 export class Prevention {
     name: string;
     image: string;
-   grave: string;
+     grave: string;
 
     constructor(n? : string, i? : string, g? : string){
         this.name = n;
