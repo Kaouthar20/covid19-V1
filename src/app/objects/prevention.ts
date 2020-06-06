1ef6f806923bea43c9af314e928d7a5e80cc9d4e
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-06 08:53:42 +0000

    modif de  nom de variables

--- src/app/objects/prevention.ts
+++ src/app/objects/prevention.ts
@@ -2,11 +2,11 @@
 export class Prevention {
     name: string;
     image: string;
-     grave: string;
+    description: string;
 
-    constructor(n? : string, i? : string, g? : string){
+    constructor(n? : string, i? : string, d? : string){
         this.name = n;
         this.image = i;
-        this.grave = g;
+        this.description = d;
     }
 }
