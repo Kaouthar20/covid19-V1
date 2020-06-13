e521f86fc44c5609fa828035e326e04debb9c046
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-13 11:49:33 +0000

    some modif on carousel component

--- src/app/component/carousel/carousel.component.ts
+++ src/app/component/carousel/carousel.component.ts
@@ -12,25 +12,25 @@ export class CarouselComponent implements OnInit {
     {
       image: '../assets/image/COVID-19-Coronavirus-Map.jpg',
       title : "Situation update worldwide",
-      badge :"Go Home",
+      badge :"read more",
       description:'Disclaimer: National updates are published at different times and in different time zones. This, and the time ECDC needs to process these data, may lead to discrepancies between the national numbers and the numbers published by ECDC'
     },
     {
       image: '../assets/image/healthService.jpg',
       title : "COVID-19 significantly impacts health services for noncommunicable diseases",
-      badge :"Go Home",
+      badge :"read more",
       description:' Prevention and treatment services for noncommunicable diseases (NCDs) have been severely disrupted since the COVID-19 pandemic began, according to a WHO survey released today.'
     },
     {
       image: '../assets/image/backgroundHome.jpg',
       title : "Mutation Allows Coronavirus to Infect More Cells, Study Finds. Scientists Urge Caution.",
-      badge :"Go Home",
+      badge :"read more",
       description:'Geneticists said more evidence is needed to determine if a common genetic variation of the virus spreads more easily between people.'
     },
     {
       image: '../assets/image/backgroundHome.jpg',
       title : "card 4",
-      badge :"Go Home",
+      badge :"read more",
       description:'provide some description here to make it awesome'
     },
     {
@@ -42,19 +42,19 @@ export class CarouselComponent implements OnInit {
     {
       image: '../assets/image/backgroundHome.jpg',
       title : "card 6",
-      badge :"Go Home",
+      badge :"read more",
       description:'provide some description here to make it awesome'
     },
     {
       image: '../assets/image/backgroundHome.jpg',
       title : "card 7",
-      badge :"Go Home",
+      badge :"read more",
       description:'provide some description here to make it awesome'
     },
     {
       image: '../assets/image/backgroundHome.jpg',
       title : "card 8",
-      badge :"Go Home",
+      badge :"read more",
       description:'provide some description here to make it awesome'
     }
   ]; 
