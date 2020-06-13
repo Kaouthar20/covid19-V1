13aa69201591395a347468fc224fbdc3c550adc3
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-13 16:29:09 +0000

    modif on ccs carousel

--- src/app/component/carousel/carousel.component.ts
+++ src/app/component/carousel/carousel.component.ts
@@ -20,50 +20,37 @@ export class CarouselComponent implements OnInit {
       image: '../assets/image/healthService.jpg',
       title : "COVID-19 significantly impacts health services for noncommunicable diseases",
       badge :"read more",
-      readmoreLink : "",
+      readmoreLink : "https://www.who.int/news-room/detail/01-06-2020-covid-19-significantly-impacts-health-services-for-noncommunicable-diseases",
       description:' Prevention and treatment services for noncommunicable diseases (NCDs) have been severely disrupted since the COVID-19 pandemic began, according to a WHO survey released today.'
     },
     {
-      image: '../assets/image/backgroundHome.jpg',
+      image: '../assets/image/mutation.jpg',
       title : "Mutation Allows Coronavirus to Infect More Cells, Study Finds. Scientists Urge Caution.",
       badge :"read more",
-      readmoreLink : "",
+      readmoreLink : 'https://www.nytimes.com/2020/06/12/science/coronavirus-mutation-genetics-spike.html?rref=briefing&module=Ribbon&version=context&region=Header&action=click&contentCollection=Top%20Stories&pgtype=Multimedia',
       description:'Geneticists said more evidence is needed to determine if a common genetic variation of the virus spreads more easily between people.'
     },
     {
-      image: '../assets/image/backgroundHome.jpg',
-      title : "card 4",
+      image: '../assets/image/bejine.jpg',
+      title : "Beijing reimposes lockdown measures after new Covid-19 outbreak",
       badge :"read more",
-      readmoreLink : "",
-      description:'provide some description here to make it awesome'
+      readmoreLink : "https://www.theguardian.com/world/2020/jun/13/beijing-china-new-covid-19-cases-linked-to-food-market",
+      description:'Parts of Beijing have reimposed lockdown measures after a cluster of locally transmitted coronavirus cases emerged nearly two months after the Chinese capital appeared to have stamped out the virus.'
     },
     {
-      image: '../assets/image/backgroundHome.jpg',
-      title : "card 5",
+      image: '../assets/image/secondwave.jpg',
+      title : "What ICU doctors have learned about COVID-19 — and how they're prepared for a 2nd wave",
       badge :"Go Home",
-      readmoreLink : "",
-      description:'provide some description here to make it awesome'
+      readmoreLink : "https://www.nbcnews.com/health/health-news/what-icu-doctors-have-learned-about-covid-19-how-they-n1225801",
+      description:'The World Health Organization had just declared COVID-19 a pandemic when intensive care units in the United States started to see an influx of severely ill patients.'
     },
+   
     {
-      image: '../assets/image/backgroundHome.jpg',
-      title : "card 6",
+      image: '../assets/image/internationalTravel.jpg',
+      title : "How can international travel resume during the coronavirus pandemic?",
       badge :"read more",
-      readmoreLink : "",
-      description:'provide some description here to make it awesome'
-    },
-    {
-      image: '../assets/image/backgroundHome.jpg',
-      title : "card 7",
-      badge :"read more",
-      readmoreLink : "",
-      description:'provide some description here to make it awesome'
-    },
-    {
-      image: '../assets/image/backgroundHome.jpg',
-      title : "card 8",
-      badge :"read more",
-      readmoreLink : "",
-      description:'provide some description here to make it awesome'
+      readmoreLink : "https://www.newscientist.com/article/2246024-how-can-international-travel-resume-during-the-coronavirus-pandemic/",
+      description:'Much of the world is starting to open up again, with many countries easing or planning to ease coronavirus travel restrictions. But would-be travellers face a confusing, uncertain and fast-changing situation.'
     }
   ]; 
   constructor() { }
@@ -93,15 +80,7 @@ export class CarouselComponent implements OnInit {
     }
   };
 
-  images = [
-    
-    '../assets/image/backgroundHome.jpg',
-    '../assets/image/backgroundHome.jpg',
-    '../assets/image/backgroundHome.jpg',
-    '../assets/image/backgroundHome.jpg',
-   
-    '../assets/image/backgroundHome.jpg'
-  ];
+  
 }
 
  
