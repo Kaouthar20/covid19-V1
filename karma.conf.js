793b3b3c2ef6771ba35403bdcac8f89dbd03748a
kaouthar <kaouthar.elouatri@gmail.com>
2020-06-13 00:09:15 +0000

    carousel modif

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/covid19-V1'),
+      dir: require('path').join(__dirname, './coverage/covid19-v1'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
